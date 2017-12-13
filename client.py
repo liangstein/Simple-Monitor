40a9f7093fe131a6b000e9863e3c9aa057c61a28
Xiao <lxxhlb@gmail.com>
2017-12-13 23:23:11 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -7,16 +7,28 @@ def client_monitor():
     list_gpu=list_gpu.read()
     list_cpu=list_cpu.read()
     hostname=hostname.read()
-    #result="\n".join(str(k) for k in list)
+    gpu_info=list_gpu.split("\n")[1:-1]
+    number_of_gpus=len(gpu_info)
+    gpu_temps=[]
+    for i in range(number_of_gpus):
+        ele=gpu_info[i].split(",")
+        gpu_temps.append(float(ele[3]))
+    gpu_states=""
+    for i in range(number_of_gpus):
+        if gpu_temps[i]<=75:
+            gpu_states="ok"
+        else:
+            gpu_states="no"
+            break
     result="Hostname: "+hostname+"\n"+"CPU states:\n"+list_cpu+"\n"+"GPU states:\n"+list_gpu+"\n"
-    return result,hostname
+    return result,gpu_states,hostname
 
 def client_main(dir):
     while 1:
         monitor_result=client_monitor()
         host_name=monitor_result[-1].replace("\n","")
         with open(dir+str(host_name),"w") as f:
-            f.write(str(monitor_result[0]))
+            f.write(str(monitor_result[0])+str(monitor_result[1]))
         time.sleep(5)
 
 if __name__=="__main__":
--- http_server.py
+++ http_server.py
@@ -12,11 +12,19 @@ def treat_as_plain_text(response):
 def index():
     hosts = os.listdir(main_dir + "/hosts_data/")
     total_states_string = ""
+    gpu_temp_check_list=[]
     for host in hosts:
         with open(main_dir + "hosts_data/" + host, "r") as f:
             node_states = f.read()
-        total_states_string += node_states
-    return  total_states_string
+        total_states_string += node_states[:-2]
+        gpu_temp_check_list.append(node_states[-2:])
+    gpu_warning=""
+    for i in range(len(gpu_temp_check_list)):
+        if gpu_temp_check_list[i]!="ok":
+            gpu_warning="There is at least one GPU too hot on "+str(hosts[i])
+        else:
+            gpu_warning="All GPUs are in proper temperature conditions! "
+    return  total_states_string+"\n"+gpu_warning+"\n"
 
 if __name__=="__main__":
     app.run(debug=1,host='0.0.0.0',port=8002)
