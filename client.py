4a1ad132aa6f4cd047aa643ddeb2f2242e1f9803
Xiao <lxxhlb@gmail.com>
2017-12-17 19:20:57 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,34 +1,44 @@
 import os
 import time
+import pickle
+from datetime import datetime
 def client_monitor():
-    list_gpu=os.popen("nvidia-smi --query-gpu=index,timestamp,power.draw,temperature.gpu,clocks.sm,clocks.mem,clocks.gr --format=csv")
-    list_cpu=os.popen("sensors")
+    list_gpu=os.popen("nvidia-smi --query-gpu=gpu_name,index,power.draw,temperature.gpu --format=csv")
+    #list_cpu=os.popen("sensors")
     hostname=os.popen("hostname")
     list_gpu=list_gpu.read()
-    list_cpu=list_cpu.read()
+    #list_cpu=list_cpu.read()
     hostname=hostname.read()
+    now=datetime.now()
+    year=now.year
+    month=now.month
+    day=now.day
+    hour=now.hour
+    minute=now.minute
+    second=now.second
     gpu_info=list_gpu.split("\n")[1:-1]
     number_of_gpus=len(gpu_info)
-    gpu_temps=[]
+    gpu_information={}
+    repeat_count=1
     for i in range(number_of_gpus):
         ele=gpu_info[i].split(",")
-        gpu_temps.append(float(ele[3]))
-    gpu_states=""
-    for i in range(number_of_gpus):
-        if gpu_temps[i]<=75:
-            gpu_states="ok"
+        if i>0:
+            if ele[0]!=gpu_info[i-1].split(",")[0]:
+                gpu_information[ele[0]]=(float(ele[-2].replace("W","")),float(ele[-1]))
+            else:
+                gpu_information[ele[0]+" "+str(repeat_count)]=(float(ele[-2].replace("W","")),float(ele[-1]))
+                repeat_count+=1
         else:
-            gpu_states="no"
-            break
-    result="Hostname: "+hostname+"\n"+"CPU states:\n"+list_cpu+"\n"+"GPU states:\n"+list_gpu+"\n"
-    return result,gpu_states,hostname
+            gpu_information[ele[0]] = (float(ele[-2].replace("W", "")), float(ele[-1]))
+    gpu_information["time"]=str(year)+str(month)+str(day)+str(hour)+str(minute)+str(second)
+    return hostname,gpu_information
 
 def client_main(dir):
     while 1:
         monitor_result=client_monitor()
-        host_name=monitor_result[-1].replace("\n","")
-        with open(dir+str(host_name),"w") as f:
-            f.write(str(monitor_result[0])+str(monitor_result[1]))
+        host_name=monitor_result[0].replace("\n","")
+        with open(dir+str(host_name),"wb") as f:
+            pickle.dump(monitor_result[-1],f,protocol=pickle.HIGHEST_PROTOCOL)
         time.sleep(5)
 
 if __name__=="__main__":
--- http_server.py
+++ http_server.py
@@ -1,4 +1,7 @@
 import os
+import pickle
+import numpy as np
+from datetime import datetime
 from flask import  Flask,Response
 app=Flask(__name__)
 main_dir="/opt/simple_monitor/"
@@ -11,20 +14,44 @@ def treat_as_plain_text(response):
 @app.route("/")
 def index():
     hosts = os.listdir(main_dir + "/hosts_data/")
-    total_states_string = ""
-    gpu_temp_check_list=[]
+    normal_states_string = ""
+    caution_states_string=""
+    now=datetime.now()
+    year=now.year
+    month=now.month
+    day=now.day
+    hour=now.hour
+    minute=now.minute
+    second=now.second
+    now_time=str(year) + str(month) + str(day) + str(hour) + str(minute) + str(second)
+    lost_nodes=[]
+    gpu_warning_location = {}
     for host in hosts:
-        with open(main_dir + "hosts_data/" + host, "r") as f:
-            node_states = f.read()
-        total_states_string += node_states[:-2]
-        gpu_temp_check_list.append(node_states[-2:])
-    gpu_warning=""
-    for i in range(len(gpu_temp_check_list)):
-        if gpu_temp_check_list[i]!="ok":
-            gpu_warning="There is at least one GPU too hot on "+str(hosts[i])
-        else:
-            gpu_warning="All GPUs are in proper temperature conditions! "
-    return  total_states_string+"\n"+gpu_warning+"\n"
+        gpu_warning_list=[]
+        with open(main_dir+"/hosts_data/"+str(host),"rb") as f:
+            gpu_states=pickle.load(f)
+        normal_states_string+=str(host)+":\n"
+        for i in gpu_states:
+            if i!="time":
+                normal_states_string+=str(i)+": "+str(gpu_states[i])+"\n"
+                gpu_temp=gpu_states[i][-1]
+                if gpu_temp>65:
+                    gpu_warning_list.append((str(i),str(gpu_states[i])))
+        node_time=gpu_states["time"]
+        if abs(float(node_time)-float(now_time))>200:
+            lost_nodes.append(host)
+        if len(gpu_warning_list)!=0:
+            gpu_warning_location[host]=gpu_warning_list
+        normal_states_string+="\n"
+    if len(gpu_warning_location)!=0:
+        caution_states_string+="Caution! Following GPUs are too hot:\n"
+        for i in gpu_warning_location:
+            caution_states_string+=str(i)+str(gpu_warning_location[i])+"\n"
+    if len(lost_nodes)!=0:
+        caution_states_string+="Caution! Following nodes are disconnected:\n"
+        for i in lost_nodes:
+            caution_states_string+=str(i)+"\n"
+    return  normal_states_string+caution_states_string
 
 if __name__=="__main__":
     app.run(debug=1,host='0.0.0.0',port=8002)
